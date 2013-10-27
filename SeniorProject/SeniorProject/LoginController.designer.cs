7f07a07a08e0bac5b71122aaf307ce56beca5683
Flodjana Jakupllari <Ajakupllari@Ardits-MacBook-Pro.local>
2013-10-27 02:57:07 +0100

    rename changes

---
+++ SeniorProject/SeniorProject/LoginController.designer.cs
@@ -0,0 +1,40 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
+
+namespace SeniorProject
+{
+	[Register ("LoginController")]
+	partial class LoginController
+	{
+		[Outlet]
+		MonoTouch.UIKit.UITextField passwordTxtField { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextField usernameTxtField { get; set; }
+
+		[Action ("loginButtonClick:")]
+		partial void loginButtonClick (MonoTouch.Foundation.NSObject sender);
+
+		[Action ("registerButtonClick:")]
+		partial void registerButtonClick (MonoTouch.Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (passwordTxtField != null) {
+				passwordTxtField.Dispose ();
+				passwordTxtField = null;
+			}
+
+			if (usernameTxtField != null) {
+				usernameTxtField.Dispose ();
+				usernameTxtField = null;
+			}
+		}
+	}
+}
