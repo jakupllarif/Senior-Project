cc4cc4df0e517e3e603d888c1c6ae9f7dd99877e
Flodjana Jakupllari <Ajakupllari@Ardits-MacBook-Pro.local>
2013-12-04 17:05:02 -0500

    deleted inherited classes

--- SeniorProject/SeniorProject/Controllers/UtilityFunctions.cs
+++ SeniorProject/SeniorProject/Controllers/UtilityFunctions.cs
@@ -11,21 +11,6 @@ namespace SeniorProject
 		{
 		}
 
-		public override void DidReceiveMemoryWarning ()
-		{
-			// Releases the view if it doesn't have a superview.
-			base.DidReceiveMemoryWarning ();
-			
-			// Release any cached data, images, etc that aren't in use.
-		}
-
-		public override void ViewDidLoad ()
-		{
-			base.ViewDidLoad ();
-			
-			// Perform any additional setup after loading the view, typically from a nib.
-		}
-
 		public void emergencyNotification(){
 
 		}
