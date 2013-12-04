3817f73888d48d4fea49628c5e918790dd64e337
gabrielliu <gabrielliu@Gabriels-MacBook-Pro-2.local>
2013-12-04 17:15:21 -0500

    add another list

--- SeniorProject/SeniorProject/Controllers/UtilityFunctions.cs
+++ SeniorProject/SeniorProject/Controllers/UtilityFunctions.cs
@@ -1,14 +1,21 @@
 using System;
 using System.Drawing;
+using System.Collections.Generic;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using SeniorProject.Models;
 
 namespace SeniorProject
 {
-	public class UtilityFunctions
+	public static class UtilityFunctions
 	{
+		public List<RegisterModel> _data;
+		public List<DataUploadModel> _utility;
+
 		public UtilityFunctions ()
 		{
+			_data = new List<RegisterModel> ();
+			_utility = new List<DataUploadModel> ();
 		}
 
 		public void emergencyNotification(){
