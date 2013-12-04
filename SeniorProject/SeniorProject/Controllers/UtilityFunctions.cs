7a013abe784e7b57addc46041a56af8602c0f4a7
gabrielliu <gabrielliu@Gabriels-MacBook-Pro-2.local>
2013-12-04 17:01:24 -0500

    a little change

--- SeniorProject/SeniorProject/Controllers/UtilityFunctions.cs
+++ SeniorProject/SeniorProject/Controllers/UtilityFunctions.cs
@@ -5,25 +5,10 @@ using MonoTouch.UIKit;
 
 namespace SeniorProject
 {
-	public partial class UtilityFunctions : UIViewController
+	public partial class UtilityFunctions
 	{
-		public UtilityFunctions () : base ("UtilityFunctions", null)
+		public UtilityFunctions ()
 		{
-		}
-
-		public override void DidReceiveMemoryWarning ()
-		{
-			// Releases the view if it doesn't have a superview.
-			base.DidReceiveMemoryWarning ();
-			
-			// Release any cached data, images, etc that aren't in use.
-		}
-
-		public override void ViewDidLoad ()
-		{
-			base.ViewDidLoad ();
-			
-			// Perform any additional setup after loading the view, typically from a nib.
 		}
 	}
 }
